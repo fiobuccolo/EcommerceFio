f3292be7de15506b1c1d4c31adb8a97137bc72ac
fiorella buccolo <fiobuccolo@gmail.com>
2023-03-23 19:22:44 +0100

    img logo en firebase

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ export const Navbar = () => {
     return (
                 <nav className="navbar navbar-expand-lg  nav-bar fixed-top ">   
                     <div className="container-fluid">
-                        <Link  className="navbar-brand nav-link" to={'/'}><img src="./img/logo.png" width="72" height="48" alt="logo" className="d-inline-block align-text-center"/></Link>
+                        <Link  className="navbar-brand nav-link" to={'/'}><img src="https://firebasestorage.googleapis.com/v0/b/ecommercefio.appspot.com/o/logo.png?alt=media&token=515fa951-31f7-4899-aba7-4db2d776ff9a" width="72" height="64" alt="logo" className="d-inline-block align-text-center"/></Link>
                         <div className="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                                 <Categorias/>
